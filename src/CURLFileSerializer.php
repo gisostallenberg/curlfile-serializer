d844eb4dc3f77489397aa9a6517cea4ae0603177
Giso Stallenberg <gisostallenberg@gmail.com>
2017-02-09 16:34:22 +0100

    Initial commit

---
+++ src/CURLFileSerializer.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace GisoStallenberg\CURLFileSerializer;
+
+use CURLFile;
+use Serializable;
+
+/**
+ * A class to serialize a CURLFile object.
+ */
+class CURLFileSerializer implements Serializable
+{
+    /**
+     * The curlfile.
+     *
+     * @var CURLFile
+     */
+    private $curlfile;
+
+    /**
+     * Create a new CURLFileSerializer class.
+     *
+     * @param CURLFile $curlfile
+     */
+    public function __construct(CURLFile $curlfile)
+    {
+        $this->curlfile = $curlfile;
+    }
+    /**
+     * Create a new CURLFileSerializer class in a static way.
+     *
+     * @param CURLFile $curlfile
+     */
+    public static function create(CURLFile $curlfile)
+    {
+        return new static($curlfile);
+    }
+
+    /**
+     * Serialize this class.
+     *
+     * @return string
+     */
+    public function serialize()
+    {
+        $fileInfo = array(
+            'fileName' => $this->curlfile->getFileName(),
+            'mimeType' => $this->curlfile->getMimeType(),
+            'postFilename' => $this->curlfile->getPostFilename(),
+        );
+
+        return serialize($fileInfo);
+    }
+
+    /**
+     * Unserializes the class.
+     *
+     * @param string $fileInfo
+     */
+    public function unserialize($fileInfo)
+    {
+        extract(unserialize($fileInfo)); // gives $fileName, $mimeType, $postFilename
+
+        $this->curlfile = new CURLFile($fileName);
+        $this->curlfile->setMimeType($mimeType);
+        $this->curlfile->setPostFilename($postFilename);
+    }
+
+    /**
+     * Gives the CURLFile.
+     *
+     * @return CURLFile
+     */
+    public function getCURLFile()
+    {
+        return $this->curlfile;
+    }
+}
